58556d93a41e1fd57cbb08fc342b96c803e72063
AnnaGustafson2 <161380808+AnnaGustafson2@users.noreply.github.com>
2025-04-12 16:22:33 -0400

    made fridge

--- recipez/app/(tabs)/fridge.tsx
+++ recipez/app/(tabs)/fridge.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Image, Platform } from "react-native";
+import { StyleSheet, Image, Platform, View, Text, TouchableOpacity } from "react-native";
 
 import { Collapsible } from "@/components/Collapsible";
 import { ExternalLink } from "@/components/ExternalLink";
@@ -9,121 +9,153 @@ import { IconSymbol } from "@/components/ui/IconSymbol";
 
 export default function Fridge() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#D0D0D0", dark: "#353636" }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="refrigerator.fill"
-          style={styles.headerImage}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Camera</ThemedText>
-      </ThemedView>
-      <ThemedText>
-        This app includes example code to help you get started.
-      </ThemedText>
-      <Collapsible title="File-based routing">
-        <ThemedText>
-          This app has two screens:{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          and{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/explore.tsx</ThemedText>
-        </ThemedText>
-        <ThemedText>
-          The layout file in{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/_layout.tsx</ThemedText>{" "}
-          sets up the tab navigator.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/router/introduction">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Android, iOS, and web support">
-        <ThemedText>
-          You can open this project on Android, iOS, and the web. To open the
-          web version, press <ThemedText type="defaultSemiBold">w</ThemedText>{" "}
-          in the terminal running this project.
-        </ThemedText>
-      </Collapsible>
-      <Collapsible title="Images">
-        <ThemedText>
-          For static images, you can use the{" "}
-          <ThemedText type="defaultSemiBold">@2x</ThemedText> and{" "}
-          <ThemedText type="defaultSemiBold">@3x</ThemedText> suffixes to
-          provide files for different screen densities
-        </ThemedText>
-        <Image
-          source={require("@/assets/images/react-logo.png")}
-          style={{ alignSelf: "center" }}
-        />
-        <ExternalLink href="https://reactnative.dev/docs/images">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Custom fonts">
-        <ThemedText>
-          Open <ThemedText type="defaultSemiBold">app/_layout.tsx</ThemedText>{" "}
-          to see how to load{" "}
-          <ThemedText style={{ fontFamily: "SpaceMono" }}>
-            custom fonts such as this one.
-          </ThemedText>
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/versions/latest/sdk/font">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Light and dark mode components">
-        <ThemedText>
-          This template has light and dark mode support. The{" "}
-          <ThemedText type="defaultSemiBold">useColorScheme()</ThemedText> hook
-          lets you inspect what the user's current color scheme is, and so you
-          can adjust UI colors accordingly.
-        </ThemedText>
-        <ExternalLink href="https://docs.expo.dev/develop/user-interface/color-themes/">
-          <ThemedText type="link">Learn more</ThemedText>
-        </ExternalLink>
-      </Collapsible>
-      <Collapsible title="Animations">
-        <ThemedText>
-          This template includes an example of an animated component. The{" "}
-          <ThemedText type="defaultSemiBold">
-            components/HelloWave.tsx
-          </ThemedText>{" "}
-          component uses the powerful{" "}
-          <ThemedText type="defaultSemiBold">
-            react-native-reanimated
-          </ThemedText>{" "}
-          library to create a waving hand animation.
-        </ThemedText>
-        {Platform.select({
-          ios: (
-            <ThemedText>
-              The{" "}
-              <ThemedText type="defaultSemiBold">
-                components/ParallaxScrollView.tsx
-              </ThemedText>{" "}
-              component provides a parallax effect for the header image.
-            </ThemedText>
-          ),
-        })}
-      </Collapsible>
-    </ParallaxScrollView>
+    <View style={styles.background}>
+      <View style={styles.content}>
+        <Text style={styles.heading}>Your Fridge</Text>
+        <View style={styles.itemBox}>
+          <View style={styles.itemRow}>
+            <Text style={styles.itemTitle}>Eggs</Text>
+            <View style={styles.itemEditContainer}> 
+            <Text style={styles.itemAmount}> x10</Text>
+            <View style={styles.triangleContainer}>
+              <TouchableOpacity onPress={() => {}}>
+                <View style={styles.upTriangle} />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => {}}>
+                <View style={styles.downTriangle} />
+              </TouchableOpacity>
+            </View>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.buttonRow}>
+        <TouchableOpacity onPress={() => {}}>
+          <View style={styles.addItemBox}>
+            <Text style={styles.addItemBoxText}>Add With Receipt</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}}>
+          <View style={styles.plusButton}>
+            <Text style={styles.plusText}>+</Text>
+          </View>
+        </TouchableOpacity>
+          </View>
+      </View>
   );
 }
 
+
 const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginTop: 50,
+    fontFamily: 'Poppins',
+    padding: 20,
   },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
+  content: {
+    flex: 1,
   },
+  background: {
+    flex: 1,
+    backgroundColor: '#FFFCF8',
+  },
+  itemBox: {
+    backgroundColor: '#FFE2C6',
+    paddingVertical: 12,
+    paddingHorizontal: 18,
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: 20
+  },
+  itemRow: {
+    flexDirection: 'row',
+    gap: 230,
+    justifyContent: 'flex-end',
+    alignItems: 'center', 
+  },
+  itemTitle: {
+    fontSize: 20,
+    fontFamily: 'Poppins',
+    color: '#000000',
+  },
+  itemAmount: {
+    fontSize: 16,
+    fontFamily: 'Poppins',
+    color: '#000000',
+  },
+  itemEditContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10
+  },
+  triangleContainer: {
+    flexDirection: 'column',
+    gap: 5,
+  },
+  upTriangle: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderLeftWidth: 5,
+    borderRightWidth: 5,
+    borderBottomWidth: 10,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#000000',
+  },
+  downTriangle: {
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderLeftWidth: 5,
+    borderRightWidth: 5,
+    borderBottomWidth: 10,
+    borderLeftColor: 'transparent',
+    borderRightColor: 'transparent',
+    borderBottomColor: '#000000',
+    transform: [{ rotate: '180deg' }],
+  },
+  addItemBox: {
+    backgroundColor: '#DA7635',
+    paddingVertical: 6,
+    paddingHorizontal: 12,
+    borderRadius: 10,
+    alignSelf: 'center',
+    marginRight: 10
+  },
+  addItemBoxText: {
+    color: '#FFFCF8',
+    fontSize: 20,
+    fontFamily: 'Poppins',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center', 
+    marginBottom: 100,
+    marginRight: 50
+  },
+  plusButton: {
+    backgroundColor: '#FFFCF8',
+    width: 50,
+    height: 50,
+    borderRadius: 25, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderWidth: 2,
+    borderColor: '#000000',
+  },
+  plusText: {
+    color: '#000000', 
+    fontSize: 36,
+    lineHeight: 36,
+    fontWeight: 'bold',
+  },
+  
 });
